672b9f16f887199faae99bed67e86e05fda20c35
Sikha Njanaseelan <sikhanjanaseelan@gmail.com>
2026-08-03 15:05:31 +0200

    feat(core): add plugin lifecycle and main loader 1.0

---
+++ wp-content/plugins/myportfolio-core/includes/class-plugin.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Core plugin loader.
+ *
+ * @package MyPortfolioCore
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Main plugin class.
+ */
+final class MyPortfolio_Core_Plugin {
+
+	/**
+	 * Initialise plugin.
+	 *
+	 * @return void
+	 */
+	public function run(): void {
+
+		/*
+		 * Future initialisation.
+		 */
+
+	}
+}
